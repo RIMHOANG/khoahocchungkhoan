13d20294434af0ec4b5586e51c4bcea4cb0267dc
ThanhNhan <hotanthanhnhan@gmail.com>
2023-01-01 16:00:17 +0700

    demo pop up

--- assets/js/sys.js
+++ assets/js/sys.js
@@ -1,3 +1,15 @@
 // bao
 
 // trung
+
+// function test() {
+//     Email.send({
+//         SecureToken : "d8aaa6e7-2940-4327-a25f-1d35d6c6873d",
+//         To : 'nhanhtt@irtech.com.vn',
+//         From : "nhanhttde150039@fpt.edu.vn",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
